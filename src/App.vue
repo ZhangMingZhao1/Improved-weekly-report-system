118f3e2d3f2cb0ca122e0d211900d1987e783626
ZhangMingZhao1 <1104272319@qq.com>
2018-11-26 21:51:22 +0800

    基本架子搭好

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+		<div> 
+			<ShowBox />
+		</div>
+		<div class="tree">
+			<Tree /> 
+		</div>
+  </div>
+</template>
+
+<script>
+import ShowBox from './components/showbox/index.vue';
+import Tree from './components/tree/index.vue';
+
+export default {
+  name: 'app',
+  components: {
+		ShowBox,
+		Tree
+  }
+}
+</script>
+
+<style>
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
+	.tree {
+		margin-top: 10px;
+	}
+</style>
---
+++ src/components/showbox/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="showBox">
+    <el-card class="box-card">
+    <div slot="header" class="clearfix">
+        <span>周报</span>
+        <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+    </div>
+    <div v-for="o in 4" :key="o" class="text item">
+        {{'列表内容 ' + o }}
+    </div>
+    </el-card>
+  </div>
+</template> 
+
+<script>
+export default {
+  name: 'ShowBox',
+  props: {
+    // msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 100%;
+  }
+</style>
